af3fa2c8f22f500b197504cb8a9b8d6266e25471
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-02-07 09:56:36 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripFollower2::Application.config.secret_token = '0f3226819efb8fe6ea7673fa0dd03a9e9955e92e0f099f664db10656d064fae350d17fe537562b7071fbf345750ee7e5d584bf74668b2d8a850b6f6aa079cf0b'
