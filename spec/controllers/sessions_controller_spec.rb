5704b2395ad3e5d98da7033fed2ff87a322c5a8f
Erik Trautman <erik.trautman@gmail.com>
2013-02-08 18:34:23 -0800

    update readme with TODO, controller test session destroy bug

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe SessionsController do
 
+  describe "#destroy" do
 
+    before do
+      session[:return_to] = new_life_thread_path
+      delete :destroy
+    end
 
+    it "destroys the session[:return_to]" do
+      session[:return_to].should be_nil
+    end
+  end
 end
